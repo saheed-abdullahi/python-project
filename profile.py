022ecc695090834aa9f28fde51ba8ffb2c1e6bdb
oniselada <oniselada@gmail.com>
2026-03-28 06:32:20 -0700

    added new project files

---
+++ profile.py
@@ -0,0 +1,4 @@
+print("my name is SAHEED ABDULLAHI OLADIMEJI")
+print("I'm student of crescent university,Abeokuta")
+print("I'm computer science student of 200l")
+print("I want to focus on machine learning project")
