39f6016756245d08d589f7e38a9ff22412c5a4e7
oniselada <oniselada@gmail.com>
2026-03-28 07:22:54 -0700

    added some new project files

--- project1.py
+++ project1.py
@@ -1,12 +1,15 @@
-print("Hi, how are you")
-n1 = int(input("enter first number: "))
-n2 = int(input("enter second number: "))
-n3 = int(input("enter third number: "))
-n4 =int( n1+n2+n3)
-n5 = n1-n2-n3
-n6 = n1*n2*n3
-print("The addition of the three numbers is: " +str(n4))
-print("The subtraction of the three numbers is: "+str(n5))
-print("The multiplication of the three numbers is: "+str(n6))
+print("let do some mathimatical calculation using  pyton")
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+n3 = int(input("Enter third number: "))
+addition = n1+n2+n3
+subtraction = n1-n2-n3
+multiplication = n1*n2*n3
+division = n1/n2/n3
+print("The addition of the three number is: "+str(addition))
+print("The subtraction of the three number is "+str(subtraction))
+print("The multiplication of the three number is "+str( multiplication))
+print("The division of the three number is: "+str(division))
+
 
 
