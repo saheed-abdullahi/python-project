5e68626844e53e5569fa7947bf944598c92614d9
oniselada <oniselada@gmail.com>
2026-03-28 07:25:44 -0700

    added mathematical operations files

--- mathematical operation.py
+++ mathematical operation.py
@@ -10,6 +10,8 @@ print("The addition of the three number is: "+str(addition))
 print("The subtraction of the three number is "+str(subtraction))
 print("The multiplication of the three number is "+str( multiplication))
 print("The division of the three number is: "+str(division))
+print("Goodbye")
+
 
 
 
