5b3233fc5161a9f28ad9b46ddd8afbfc43e2b44c
oniselada <oniselada@gmail.com>
2026-03-28 06:53:52 -0700

    add

--- project1.py
+++ project1.py
@@ -1,5 +1,12 @@
-n1 = input("enter first number: ")
-n2 = input("enter second number: ")
-n3 = input("enter third number: ")
-n4 = n1+n2+n3
-print(n4)
+print("Hi, how are you")
+n1 = int(input("enter first number: "))
+n2 = int(input("enter second number: "))
+n3 = int(input("enter third number: "))
+n4 =int( n1+n2+n3)
+n5 = n1-n2-n3
+n6 = n1*n2*n3
+print("The addition of the three numbers is: " +str(n4))
+print("The subtraction of the three numbers is: "+str(n5))
+print("The multiplication of the three numbers is: "+str(n6))
+
+
