b33cf927cfe721ddc285e368dd2649e7f5bbde32
oniselada <oniselada@gmail.com>
2026-03-30 14:03:15 -0700

    added some new project to my file

---
+++ admission.py
@@ -0,0 +1,9 @@
+# write A python program to take marks from the user to check whether user able to grant 
+# admission in college or not. if marks is less than 60 then it don't allow to take
+#  admission 
+name = input("Enter your name: ")
+score = int(input('Enter your scores: '))
+if(score >= 60):
+    print("congratulations! " +name+ " you are grant for the admission to our college(college of health)")
+else:
+    print("sorry fellow applicant," +name+ ", your score is "+str(score)+ " and it is less than our cut of mark. you need " +str(60-score)+ " to allow for the addmission")
---
+++ vote.py
@@ -0,0 +1,14 @@
+# Write a python program to take age from the user to check whether user able to 
+# participate in voting or not. if age is less than 18 then it does't allow to 
+# participate. and show , after how much year a person will be able to participate
+# in voting
+# Expected result if user input 10 years then:
+# sorry! you cannot participate in voting, you will be able to participate after 8 year
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+if (age>=18):
+    print ("Hi "+name+ " you can take part in the election")
+else:
+    print( "hi " +name+" you cannot participate in the election because your age is "+str(age)+ "years, you will wait for " +str(18-age)+ " year")
+
+
